9aec5c89315aa34cb70f8debc437679fa0311294
Steve Vaught <Steven.D.Vaught@gmail.com>
2011-03-09 20:04:21 -0500

    moved text functions to helpers.py

--- twittshell.py
+++ twittshell.py
@@ -42,6 +42,7 @@ import time
 import twitter
 import sys
 import getopt
+from helpers import *
 
 #default: python50 my test account
 consumer_key = '2x0h71VcEAk2dAdf886HmA';
@@ -71,69 +72,6 @@ version_number = "0.14";
 #printl appends carriage returns ...
 def printl(string):
 	print (string+"\r");
-
-#------------------------------------------------------------
-#usage text
-def usage():
-	printl("");
-	printl("Usage: twitshell.py [-p message_content] [-r]");
-	printl("Options:");
-	printl("	-h		Show this help and exit --help");
-	printl("	-v		Version information --version");
-	printl("	-p [message]	Post a tweet containing [message] --post");
-	printl("	-r		Read(and Display) recent replies --read");
-
-#------------------------------------------------------------
-#about text
-def about():
-	printl("Twitter Shell Version "+version_number);
-	printl("	By Jason White, February 20 2011");
-	printl("");
-	printl("Special Thanks to:");
-	printl("	Alphageek");
-	printl("	BatSteve");# why don't you ever add yourself ?
-	printl("	maglinvinn");
-	printl("	MrTransistor");
-	printl("	Quick_Ben");
-	printl("	Sparky Projects");
-	printl("	Tmb");
-	printl("	Tysk");
-	printl("	and everyone else in the forums !");
-	printl("");
-	printl("This program takes the output of a Teletype machine");
-	printl("and allows the user to post it to twitter. It is a ");
-	printl("fairly simple python script using the python-twitter");
-	printl("module to interface with twitter");
-	printl("");
-	printl("This Program is licensed under the Modified BSD license");
-	printl("	Program Copyright (C) 2011 Jason White\n");
-#------------------------------------------------------------
-#banner text
-def banner():
-	printl("	Twitter Shell Version "+version_number);
-	printl(" Because the geek shall inherit the earth");
-	printl("");
-
-#------------------------------------------------------------
-#bad command text
-def bad_command():
-	printl("Unknown command type ? or help for help");
-
-#------------------------------------------------------------
-#Help Me ! Text
-def help():
-	printl("Help Me ! - Commands:");
-	printl("");
-	printl("	[a] or about	- more info about this program");
-	printl("	[d] or delete	- delete the last message");
-	printl("	[h] or help	- this help text");
-	printl("	[r] or read	- read replies");
-	printl("	[u] or undelete	- undelete the last post, if it was deleted");
-	printl("	[p] or post	- post a message to twitter");
-	printl("	[q] or quit	- quit this program");
-	printl("");
-	printl("	Twitter Shell "+version_number+" For The Geek Group");
-	printl("	 By Jason White February 20 2011\n");
 
 #------------------------------------------------------------
 #time date functions, returns string
